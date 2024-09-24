ec7aeee245db6cc4b5ffa2f9cd5345f25ae277e3
AmmarDaruger <159748948+s223201882@users.noreply.github.com>
2024-09-24 20:13:31 +1000

    commit

--- book_catalog/config.py
+++ book_catalog/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("postgresql://s223201882part2_user:D92UYiVvfb3dyllTByWWp9pHC1Agy44D@dpg-crou4md6l47c73fitlog-a.oregon-postgres.render.com/s223201882part2")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
 settings = Settings()
